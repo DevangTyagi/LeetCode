7e22747d6574a35b94bd0d7f14481e354cfb475f
Devang Tyagi <114945626+DevangTyagi@users.noreply.github.com>
2024-09-24 11:57:54 +0530

    Time: 27 ms (67.92%) | Memory: 35.9 MB (58.48%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void InorderBST(TreeNode*root,vector<int>&ans)
+    {
+        if(root==nullptr)
+        return;
+        InorderBST(root->left,ans);
+        ans.push_back(root->val);
+        InorderBST(root->right,ans);
+    }
+
+    bool findTarget(TreeNode* root, int k) 
+    {
+        vector<int>ans;
+        InorderBST(root,ans);
+        int low=0 , high=ans.size()-1;
+        while(low<high)
+        {
+            if(ans[low]+ans[high] == k)
+            return true;
+            if(ans[low]+ans[high] < k)
+            low++;
+            else
+            high--;
+        }
+        return false;
+    }
+};
