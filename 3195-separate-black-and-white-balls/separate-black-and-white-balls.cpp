b20717038d82483bcccf919ce23b212fcc80ed81
Devang Tyagi <114945626+DevangTyagi@users.noreply.github.com>
2024-10-15 19:52:20 +0530

    Time: 27 ms (89.47%) | Memory: 15.7 MB (45.68%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long minimumSteps(string s) 
+    {
+      long long  count = 0;
+      long long ans = 0 ;
+      for(int i = s.size()-1 ; i>=0 ; i--)
+      {
+         if(s[i]=='0')
+         count++;
+         if(s[i]=='1')
+         ans += count;
+      }
+      return ans;
+    }
+};
