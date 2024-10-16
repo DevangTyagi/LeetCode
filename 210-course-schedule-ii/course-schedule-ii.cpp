5244e9d5fc89d57f9c34d4c6480abff297f3d632
Devang Tyagi <114945626+DevangTyagi@users.noreply.github.com>
2024-10-16 17:38:11 +0530

    Time: 15 ms (68.31%) | Memory: 18.1 MB (30.84%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites){
         vector<vector<int>> adj(numCourses);
-        vector<int> visited(numCourses, 0);
-        
-        for (auto pre : prerequisites) {
+        vector<int> indegree(numCourses, 0);
+        queue<int> q;
+        vector<int> order;
+        for (auto pre : prerequisites){
             int course = pre[0];
             int prereq = pre[1];
             adj[prereq].push_back(course);
-            visited[course]++;
-        }
-        
-        queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (visited[i] == 0) {
+            indegree[course]++;
+        }        
+        for (int i = 0; i < numCourses; i++){
+            if (indegree[i] == 0) {
                 q.push(i);
             }
-        }
-        
-        vector<int> order;
-        while (!q.empty()) {
+        }       
+        while (!q.empty()){
             int course = q.front();
             q.pop();
             order.push_back(course);
             
-            for (int nextCourse : adj[course]) {
-                visited[nextCourse]--;
-                if (visited[nextCourse] == 0) {
+            for (int nextCourse : adj[course]){
+                indegree[nextCourse]--;           //reducing the prerequisite courses
+                if (indegree[nextCourse] == 0){
                     q.push(nextCourse);
                 }
             }
