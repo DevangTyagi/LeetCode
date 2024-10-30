fe621b7e3d351e4396acb4ad3da8c690b88f2086
Devang Tyagi <114945626+DevangTyagi@users.noreply.github.com>
2024-10-30 16:35:44 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (89.41%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+    {
+      vector<int>dist(n,1e9);
+      // {stops , node , cost}
+      queue< pair<int , pair<int,int>>> q;
+      vector<pair<int,int>>adj[n];
+      for(auto &itr : flights){
+        adj[itr[0]].push_back({itr[1],itr[2]});
+        // {node , cost}
+      }
+      q.push({0,{src,0}});
+      dist[src] = 0;
+      while(!q.empty())
+      {
+        int stops = q.front().first;
+        int node = q.front().second.first;
+        int cost = q.front().second.second;
+        q.pop();
+        if(stops > k)
+        continue;
+        for(auto itr : adj[node])
+        {
+          int adj_node = itr.first;
+          int new_dis = itr.second;
+          if(new_dis + cost < dist[adj_node]  && stops <= k){
+            dist[adj_node] = new_dis + cost;
+            q.push({stops + 1 , { adj_node , new_dis + cost }});
+          }
+        }
+      }
+      if(dist[dst] == 1e9) return -1;
+      return dist[dst];
+    }
+};
