582db05da9c50a22461e99643d8100aead9f20d8
Devang Tyagi <114945626+DevangTyagi@users.noreply.github.com>
2024-11-16 12:13:56 +0530

    Time: 4 ms (33.33%) | Memory: 33.2 MB (32.15%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -6,9 +6,11 @@ public:
         for(int i=0;i<nums.size()-k+1;i++)
         {
             bool consecutive = true;
-            for(int j=i+1;j<i+k;j++){
-             if(nums[j] != nums[j-1]+1)
+            for(int j=i+1 ; j<i+k ; j++){
+             if(nums[j] != nums[j-1]+1){
              consecutive = false;
+             break;
+             }
             }
             if(consecutive)
             ans.push_back(nums[i+k-1]);
