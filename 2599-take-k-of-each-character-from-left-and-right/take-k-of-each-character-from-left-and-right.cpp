2bf8897f04b67948ebac57ba527ee5a79ecba2c3
Devang Tyagi <114945626+DevangTyagi@users.noreply.github.com>
2024-11-20 13:18:14 +0530

    Time: 58 ms (23.42%) | Memory: 13.5 MB (16.15%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     int takeCharacters(string s, int k) {
       unordered_map<char,int>mpp;
-      unordered_map<char,int>freq;
+      vector<int> freq(3, 0);
       int maxlen = 0;
       for(char &it : s){
         mpp[it]++;
       } 
-      if(k == 0)
-      return 0;
       if (mpp['a'] < k || mpp['b'] < k || mpp['c'] < k)
       return -1;
       int i=0,j = 0;
-      while(j < s.size() && i < s.size()){
-       freq[s[j]]++;
-       while(freq[s[j]] > mpp[s[j]] - k && i < s.size()){
-        freq[s[i]]--;
+      while(j < s.size()){
+       freq[s[j] - 'a']++;
+       while(freq[0] > mpp['a'] - k || freq[1] > mpp['b'] - k || freq[2] > mpp['c'] - k){
+        freq[s[i] - 'a']--;
         i++;
        }
-       if(j >= i)
        maxlen = max(maxlen,j-i+1);
        j++;
       }
