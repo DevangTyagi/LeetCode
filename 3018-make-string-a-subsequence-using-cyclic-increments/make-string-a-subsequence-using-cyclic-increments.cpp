e21dc45a793827ba564bcf8fda05babae42d2ba8
Devang Tyagi <114945626+DevangTyagi@users.noreply.github.com>
2024-12-04 22:50:41 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (13.81%) - LeetSync

---
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int n = str1.size(), m = str2.size();
+        int j = 0;        
+        for (int i = 0; i < n && j < m;++i){
+            if (str1[i] == str2[j]){
+                j++;
+            }
+            else if ((str1[i] - 'a' + 1) % 26 + 'a' == str2[j]){
+                j++;
+            }
+        }
+        return j == m;
+    }
+};
