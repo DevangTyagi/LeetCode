724ae44029bda8393f2ad29635f92644c2ff340c
Devang Tyagi <114945626+DevangTyagi@users.noreply.github.com>
2024-12-08 23:39:27 +0530

    Time: 10 ms (22.22%) | Memory: 25.8 MB (11.11%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -6,12 +6,7 @@ public:
         vector<int>ans(n,0);
         for(int i=0;i<n;i++){
             int k = nums[i];
-            if(nums[i] > 0)
-            ans[i] = nums[(i + k)%n];
-            else if(nums[i] < 0 && k < n)
             ans[i] = nums[(i + k % n + n) % n];
-            else
-            ans[i] = 0;
         }
         return ans;
     }
