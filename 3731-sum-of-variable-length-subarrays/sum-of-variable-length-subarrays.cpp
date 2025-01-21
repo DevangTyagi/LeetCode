37d31f430c04664d53e08dd2e42a8ea6966c3b75
Devang Tyagi <114945626+DevangTyagi@users.noreply.github.com>
2025-01-21 10:13:49 +0530

    Time: 2 ms (19.60%) | Memory: 26.8 MB (34.36%) - LeetSync

---
+++ 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums) {
+        int maxsum = 0;
+        for(int i=0;i<nums.size();i++){
+            int start = max(0,i-nums[i]);
+            int temp_sum=0;
+            if(start<=i){
+                for(int j=start;j<=i;j++)
+                temp_sum += nums[j];
+            }
+            maxsum += temp_sum;
+        }
+        return maxsum;
+    }
+};
