61e8bac8c02fdbeb973f81c401f9e186fce934f9
Devang Tyagi <114945626+DevangTyagi@users.noreply.github.com>
2025-01-23 10:03:15 +0530

    Time: 4 ms (44.09%) | Memory: 26.5 MB (73.49%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -3,29 +3,21 @@ public:
     int countServers(vector<vector<int>>& grid) {
         vector<int>row_sum(grid.size(),0);
         vector<int>col_sum(grid[0].size(),0);
-        int total_server = 0;
         int isolated = 0;
         for(int i=0;i<grid.size();i++){
-            row_sum[i] = accumulate(grid[i].begin(),grid[i].end(),0);
-        }
-        for (int j = 0; j < grid[0].size(); j++) {
-            for (int i = 0; i < grid.size(); i++) {
-              col_sum[j] += grid[i][j];
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]){
+                    row_sum[i]++;
+                    col_sum[j]++;
+                }
             }
-        }
-        for (auto &row : grid){
-           for (int val : row){ 
-               if (val == 1) {
-               total_server++;
-            }
-         }
-}
+       }		
         for(int i =0 ;i <grid.size();i++) {
             for(int j=0;j<grid[0].size();j++) {
-                if(grid[i][j] != 0 && row_sum[i] == 1 && col_sum[j]==1)
+                if(grid[i][j] != 0 && (row_sum[i] > 1 || col_sum[j] > 1))
                 isolated++;
             }
         }
-        return total_server - isolated;
+        return isolated;
     }
 };
