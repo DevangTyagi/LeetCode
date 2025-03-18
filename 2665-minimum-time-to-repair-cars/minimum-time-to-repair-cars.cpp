8ab6601c65f7790b13f5a65aa6b7f851046f7bcb
Devang Tyagi <114945626+DevangTyagi@users.noreply.github.com>
2025-03-18 09:00:26 +0530

    Time: 60 ms (27.44%) | Memory: 59.4 MB (49.37%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    long long repairCars(vector<int>& ranks, int cars) {
-        long long left = 0, right = 1LL *
-                                    *min_element(ranks.begin(), ranks.end()) *
-                                    cars * cars;
-
-        while (left < right) {
-            long long mid = left + (right - left) / 2;
-
-            if (canRepairInTime(ranks, cars, mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return left;
+    long long Cars_repair(vector<int>& ranks, long long avl_time) {
+        long long sum = 0;
+        for (int i = 0; i < ranks.size(); i++)
+            sum += sqrt(avl_time / ranks[i]);
+        return sum;
     }
 
-private:
-    bool canRepairInTime(vector<int>& ranks, int cars, long long time) {
-        long long totalCars = 0;
+    long long repairCars(vector<int>& ranks, int cars) {
+        long long l = 1,
+                  r = (long long)*max_element(ranks.begin(), ranks.end()) * (long long) cars * cars;
+       long long ans = r;
+        while (l <= r) {
+            long long mid = l + (r - l) / 2;
+            long long sum = Cars_repair(ranks,mid);
+            if (sum >= cars) {
+                ans = min(ans, mid);
+                 r = mid - 1;
+            }
 
-        for (int rank : ranks) {
-            long long maxCars = sqrt(time / rank);
-            totalCars += maxCars;
-
-            if (totalCars >= cars)
-                return true;
+            else
+                l = mid + 1;
         }
-
-        return false;
+        return ans;
     }
 };
